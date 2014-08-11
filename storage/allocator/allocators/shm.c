2c3827174e3d5dbe891c3eac7eca69574958f732
牛海青 <haiqing1@staff.sina.com.cn>
2014-08-11 14:40:34 +0800

    Modified shm.c for the simpler allocator.

--- storage/allocator/allocators/shm.c
+++ storage/allocator/allocators/shm.c
@@ -37,142 +37,45 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-typedef struct  {
-    yac_shared_segment common;
-    int shm_id;
-} yac_shared_segment_shm;
+static yac_shared_memory_t attach(unsigned long size) /* {{{ */ {
+	yac_shared_memory_t ret;
+	unsigned int pagesize, real_size;
 
-static int create_segments(size_t k_size, size_t v_size, yac_shared_segment_shm **shared_segments_p, int *shared_segments_count, char **error_in) /* {{{ */ {
-	struct shmid_ds sds;
-	int shm_id, shmget_flags;
-	yac_shared_segment_shm *shared_segments, first_segment;
-    unsigned int i, j, allocate_size, allocated_num, segments_num, segment_size;
+	pagesize = sysconf(_SC_PAGE_SIZE);
 
-	shmget_flags = IPC_CREAT|SHM_R|SHM_W|IPC_EXCL;
-    segments_num = 1024;
-    while ((v_size / segments_num) < YAC_SMM_SEGMENT_MIN_SIZE) {
-        segments_num >>= 1;
-    }
-    segment_size = v_size / segments_num;
-    allocate_size = YAC_SMM_SEGMENT_MAX_SIZE;
+	if (size%pagesize!=0) {
+		size = (size/pagesize+1)*pagesize;
+	}
 
-    while ((shm_id = shmget(IPC_PRIVATE, allocate_size, shmget_flags)) < 0) {
-        allocate_size >>= 1;
-    }
+	ret.allocator_info.i = shmget(IPC_PRIVATE, size, 0600);
+	if (ret.allocator_info.i<0) {
+		ret.ptr = NULL;
+		ret.size = 0;
+		ret.allocator_info.i = -1;
+	} else {
+		ret.ptr = shmat(ret.allocator_info.i, NULL, 0);
+		if (ret.ptr == (void*)-1) {
+			ret.ptr = NULL;
+			ret.size = 0;
+			shmctl(ret.allocator_info.i, IPC_RMID, NULL);
+			ret.allocator_info.i = -1;
+		} else {
+			ret.size = size;
+		}
+	}
 
-    if (shm_id < 0) {
-        /* this should never happen */
-        *error_in = "shmget";
-        return 0;
-    }
-
-    if (allocate_size < YAC_SMM_SEGMENT_MIN_SIZE) {
-        /* this should never happen */
-        *error_in = "shmget";
-        return 0;
-    }
-
-    if (k_size <= allocate_size) {
-        first_segment.shm_id = shm_id;
-        first_segment.common.pos = 0;
-        first_segment.common.size = allocate_size;
-        first_segment.common.p = shmat(shm_id, NULL, 0);
-        shmctl(shm_id, IPC_RMID, &sds);
-        if (first_segment.common.p == (void *)-1) {
-            *error_in = "shmat";
-            return 0;
-        }
-    } else {
-        shmctl(shm_id, IPC_RMID, &sds);
-        *error_in = "shmget";
-        return 0;
-    }
-
-    allocated_num = (v_size % allocate_size)? (v_size / allocate_size) + 1 : (v_size / allocate_size);
-    shared_segments = (yac_shared_segment_shm *)calloc(1, (allocated_num) * sizeof(yac_shared_segment_shm));
-    if (!shared_segments) {
-        *error_in = "calloc";
-        return 0;
-    }
-
-    for (i = 0; i < allocated_num; i ++) {
-        shm_id = shmget(IPC_PRIVATE, allocate_size, shmget_flags);
-        if (shm_id == -1) {
-            *error_in = "shmget";
-            for (j = 0; j < i; j++) {
-                shmdt(shared_segments[j].common.p);
-            }
-            free(shared_segments);
-            return 0;
-        }
-        shared_segments[i].shm_id = shm_id;
-        shared_segments[i].common.pos = 0;
-        shared_segments[i].common.size = allocate_size;
-        shared_segments[i].common.p = shmat(shm_id, NULL, 0);
-        shmctl(shm_id, IPC_RMID, &sds);
-        if (shared_segments[i].common.p == (void *)-1) {
-            *error_in = "shmat";
-            for (j = 0; j < i; j++) {
-                shmdt(shared_segments[j].common.p);
-            }
-            free(shared_segments);
-            return 0;
-        }
-    }
-
-    ++segments_num;
-    *shared_segments_p = (yac_shared_segment_shm *)calloc(1, segments_num * sizeof(yac_shared_segment_shm));
-    if (!*shared_segments_p) {
-		free(shared_segments);
-        *error_in = "calloc";
-        return 0;
-    } else {
-        *shared_segments_p[0] = first_segment;
-    }
-    *shared_segments_count = segments_num;
-
-    j = 0;
-    for (i = 1; i < segments_num; i++) {
-        if (shared_segments[j].common.pos == 0) {
-            (*shared_segments_p)[i].shm_id = shared_segments[j].shm_id;
-        }
-
-        if ((shared_segments[j].common.size - shared_segments[j].common.pos) >= (2 * YAC_SMM_ALIGNED_SIZE(segment_size))) {
-            (*shared_segments_p)[i].common.pos = 0;
-            (*shared_segments_p)[i].common.size = YAC_SMM_ALIGNED_SIZE(segment_size);
-            (*shared_segments_p)[i].common.p = shared_segments[j].common.p + YAC_SMM_ALIGNED_SIZE(shared_segments[j].common.pos);
-            shared_segments[j].common.pos += YAC_SMM_ALIGNED_SIZE(segment_size);
-        } else {
-            (*shared_segments_p)[i].common.pos = 0;
-            (*shared_segments_p)[i].common.size = shared_segments[j].common.size - shared_segments[j].common.pos;
-            (*shared_segments_p)[i].common.p = shared_segments[j].common.p + YAC_SMM_ALIGNED_SIZE(shared_segments[j].common.pos);
-            j++;
-        }
-    }
-
-    free(shared_segments);
-
-	return 1;
+	return ret;
 }
-/* }}} */
 
-static int detach_segment(yac_shared_segment_shm *shared_segment) /* {{{ */ {
-    if (shared_segment->shm_id) {
-        shmdt(shared_segment->common.p);
-    }
-	return 1;
-}
-/* }}} */
-
-static size_t segment_type_size(void) /* {{{ */ {
-	return sizeof(yac_shared_segment_shm);
+static int detach(yac_shared_memory_t *shm) /* {{{ */ {
+	shmdt(shm->ptr);
+	return shmctl(shm->allocator_info.i, IPC_RMID, NULL);
 }
 /* }}} */
 
 yac_shared_memory_handlers yac_alloc_shm_handlers = /* {{{ */ {
-	(create_segments_t)create_segments,
-	(detach_segment_t)detach_segment,
-	segment_type_size
+	attach,
+	detach,
 };
 /* }}} */
 
